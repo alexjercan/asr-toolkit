1de1cf99d2be9c72267092879c2928d445348e3a
Alexandru Jercan <jercan_alex27@yahoo.com>
2021-08-11 15:48:57 +0300

    feat: stole BeamSearchDecoderWithLM from the NVidia NeMo project

--- visualisation.py
+++ visualisation.py
@@ -60,6 +60,25 @@ def plot_waveform(waveform, sample_rate, title="Waveform", xlim=None, ylim=None)
     plt.show(block=False)
 
 
+def plot_specgram(waveform, sample_rate, title="Spectrogram", xlim=None):
+  waveform = waveform.numpy()
+
+  num_channels, num_frames = waveform.shape
+  time_axis = torch.arange(0, num_frames) / sample_rate
+
+  figure, axes = plt.subplots(num_channels, 1)
+  if num_channels == 1:
+    axes = [axes]
+  for c in range(num_channels):
+    axes[c].specgram(waveform[c], Fs=sample_rate)
+    if num_channels > 1:
+      axes[c].set_ylabel(f'Channel {c+1}')
+    if xlim:
+      axes[c].set_xlim(xlim)
+  figure.suptitle(title)
+  plt.show(block=False)
+
+
 # Reference: https://github.com/imalic3/python-word-error-rate
 def print_err_html(r, h, d=None):
     x = len(r)
