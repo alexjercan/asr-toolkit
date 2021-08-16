98323bf1fa5b044d96aab59e3436dd69949ebf4c
Alexandru Jercan <jercan_alex27@yahoo.com>
2021-08-16 14:34:18 +0300

    feat: added book loader

--- datasets.py
+++ datasets.py
@@ -2,6 +2,10 @@ import os
 import torch
 import torchaudio
 
+import pandas as pd
+from pathlib import Path
+from torchaudio.datasets.librispeech import load_librispeech_item
+
 from torch.utils.data import Dataset, DataLoader, ConcatDataset
 from .general import pad_last
 
@@ -43,3 +47,36 @@ def librispeech_dataloader(dictionary, root, urls, folder_in_archive="LibriSpeec
     nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, workers])
     dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=nw, pin_memory=pin_memory, shuffle=shuffle, collate_fn=collate_fn)
     return dataset, dataloader
+
+
+class LibriSpeechBookDataset(torchaudio.datasets.LIBRISPEECH):
+    def __init__(self, root, url, folder_in_archive="LibriSpeech", download=False):
+        super(LibriSpeechBookDataset, self).__init__(root, url, folder_in_archive, download)
+
+        chapterpaths = {p.stem:str(p) for p in Path(self._path).glob('*/*/')}
+        names = ["ID", "READER", "MINUTES", "SUBSET", "PROJ.", "BOOK ID", "CH. TITLE", "PROJECT TITLE"]
+        converters = {"BOOK ID": str.strip, "SUBSET": str.strip, "CH. TITLE" : str.strip, "PROJECT TITLE" : str.strip}
+        chapters = os.path.join(root, folder_in_archive, "CHAPTERS.TXT")
+        df = pd.read_csv(chapters, delimiter='|', comment=';', names=names, converters=converters)
+        df = df[df["SUBSET"] == os.path.basename(url)].groupby("BOOK ID")
+        df = pd.DataFrame({"CHAPTERS": df["ID"].apply(list), "MINUTES": df["MINUTES"].apply(sum)})
+        df['CHAPTER_PATH'] = df.apply(lambda row: [chapterpaths[str(x)] for x in row["CHAPTERS"]], axis=1)
+        df.reset_index(level=0, inplace=True)
+        df = df.astype({"BOOK ID": 'object'})
+
+        names = ["BOOK ID", "BOOK TITLE"]
+        converters = {"BOOK ID": str.strip, "BOOK TITLE": str.strip}
+        books = os.path.join(root, folder_in_archive, "BOOKS.TXT")
+        dfp = pd.read_csv(books, delimiter='|', comment=';', names=names, converters=converters, usecols=names)
+        df = pd.merge(df, dfp, how="inner", on="BOOK ID")
+
+        self._walker = df
+
+    def __getitem__(self, n):
+        row = self._walker.iloc[n]
+
+        audiofileids = [str(p.stem) for chapterpath in row["CHAPTER_PATH"] for p in Path(chapterpath).glob('*' + self._ext_audio)]
+        items = [load_librispeech_item(fileid, self._path, self._ext_audio, self._ext_txt) for fileid in audiofileids]
+
+        waveforms, _, utterances, _, _, _ = zip(*items)
+        return torch.cat(waveforms, dim=1), " ".join(utterances), row["BOOK TITLE"], row["MINUTES"]
